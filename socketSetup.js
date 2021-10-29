1b13b0e5f2d2e3ed07235a5834a8892e31920408
AlwinJoshy <flamencoresonance@gmail.com>
2021-10-29 10:37:28 +0400

    random port mapping

--- socketSetup.js
+++ socketSetup.js
@@ -2,7 +2,7 @@ const net = require('net');
 
 const Init = () => {
 
-    const server = net.createServer((c) => {
+    const server = net.createServer( (c) => {
         // 'connection' listener.
         console.log('client connected');
         console.log("Address : " + c.remotePort);
@@ -15,7 +15,12 @@ const Init = () => {
     server.on('error', (err) => {
         throw err;
     });
-    server.listen(process.env.PORT || 51234, () => {
+
+    server.on('message', (msg) => {
+
+    });
+
+    server.listen(0, () => {
         console.log('server bound');
     });
 }
