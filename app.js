ed4441edb46263f5c5cab52bb31e7cde7fb10bb9
AlwinJoshy <flamencoresonance@gmail.com>
2021-11-01 19:03:13 +0400

    send raw message to ip 91.74.33.16

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const httpApi = require('./httpApi.js');
+const dgram = require('dgram');
 
 let dashboard = {
     serverPort: 0000,
@@ -8,9 +9,9 @@ let dashboard = {
 httpApi.Start(dashboard);
 
 //-----------------------------------------------------------
-import dgram from 'dgram';
 
-const server = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+
+const server = dgram.createSocket('udp4');
 
 server.on('error', (err) => {
     console.log(`server error:\n${err.stack}`);
@@ -18,7 +19,7 @@ server.on('error', (err) => {
 });
 
 server.on('message', (msg, rinfo) => {
-    //console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
 });
 
 server.on('listening', () => {
@@ -26,7 +27,14 @@ server.on('listening', () => {
     console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.send(Buffer.from("HAND_SHAKE_MESSAGE"), 4456, "91.74.33.16", () => { })
+setTimeout(() => {
+    setInterval(() => {
+        server.send(Buffer.from("HAND_SHAKE_MESSAGE"), 4456, "91.74.33.16", () => { })
+    }, 1000)
+}
+    , 3000)
 
-server.bind(process.env.PORT);
+
+
+//server.bind(5656);
 // Prints: server listening 0.0.0.0:41234
